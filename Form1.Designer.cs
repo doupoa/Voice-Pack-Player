ae22585e39c640574190b30abecde9d83e543ce8
Vira <2398392698@qq.com>
2022-09-13 14:08:08 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace KeyMusic
+{
+    partial class 轻音
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(轻音));
+            this.OutputDeviceList = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.isRandom = new System.Windows.Forms.CheckBox();
+            this.PlayMusic = new System.Windows.Forms.Button();
+            this.StopMusic = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.PlaybackProgress = new System.Windows.Forms.ProgressBar();
+            this.AudioStaus = new System.Windows.Forms.Label();
+            this.ScanDir = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.FileList = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // OutputDeviceList
+            // 
+            this.OutputDeviceList.FormattingEnabled = true;
+            this.OutputDeviceList.Location = new System.Drawing.Point(9, 24);
+            this.OutputDeviceList.Margin = new System.Windows.Forms.Padding(2);
+            this.OutputDeviceList.Name = "OutputDeviceList";
+            this.OutputDeviceList.Size = new System.Drawing.Size(257, 20);
+            this.OutputDeviceList.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "音频输出至:";
+            // 
+            // isRandom
+            // 
+            this.isRandom.AutoSize = true;
+            this.isRandom.Checked = true;
+            this.isRandom.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.isRandom.Location = new System.Drawing.Point(198, 6);
+            this.isRandom.Margin = new System.Windows.Forms.Padding(2);
+            this.isRandom.Name = "isRandom";
+            this.isRandom.Size = new System.Drawing.Size(72, 16);
+            this.isRandom.TabIndex = 4;
+            this.isRandom.Text = "随机播放";
+            this.isRandom.UseVisualStyleBackColor = true;
+            // 
+            // PlayMusic
+            // 
+            this.PlayMusic.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.PlayMusic.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.PlayMusic.Location = new System.Drawing.Point(141, 46);
+            this.PlayMusic.Margin = new System.Windows.Forms.Padding(2);
+            this.PlayMusic.Name = "PlayMusic";
+            this.PlayMusic.Size = new System.Drawing.Size(57, 22);
+            this.PlayMusic.TabIndex = 6;
+            this.PlayMusic.Text = "▶️播放";
+            this.PlayMusic.UseVisualStyleBackColor = true;
+            this.PlayMusic.Click += new System.EventHandler(this.PlayMusic_Click);
+            this.PlayMusic.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PlayMusic_MouseMove);
+            // 
+            // StopMusic
+            // 
+            this.StopMusic.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.StopMusic.ForeColor = System.Drawing.Color.Red;
+            this.StopMusic.Location = new System.Drawing.Point(207, 46);
+            this.StopMusic.Margin = new System.Windows.Forms.Padding(2);
+            this.StopMusic.Name = "StopMusic";
+            this.StopMusic.Size = new System.Drawing.Size(59, 22);
+            this.StopMusic.TabIndex = 7;
+            this.StopMusic.Text = "■停止";
+            this.StopMusic.UseVisualStyleBackColor = true;
+            this.StopMusic.Click += new System.EventHandler(this.StopMusic_Click);
+            this.StopMusic.MouseMove += new System.Windows.Forms.MouseEventHandler(this.StopMusic_MouseMove);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "播放列表";
+            // 
+            // PlaybackProgress
+            // 
+            this.PlaybackProgress.Location = new System.Drawing.Point(9, 186);
+            this.PlaybackProgress.Name = "PlaybackProgress";
+            this.PlaybackProgress.Size = new System.Drawing.Size(176, 12);
+            this.PlaybackProgress.TabIndex = 1;
+            // 
+            // AudioStaus
+            // 
+            this.AudioStaus.AutoSize = true;
+            this.AudioStaus.Location = new System.Drawing.Point(193, 186);
+            this.AudioStaus.Name = "AudioStaus";
+            this.AudioStaus.Size = new System.Drawing.Size(71, 12);
+            this.AudioStaus.TabIndex = 12;
+            this.AudioStaus.Text = "00:00/00:00";
+            // 
+            // ScanDir
+            // 
+            this.ScanDir.Location = new System.Drawing.Point(68, 46);
+            this.ScanDir.Margin = new System.Windows.Forms.Padding(2);
+            this.ScanDir.Name = "ScanDir";
+            this.ScanDir.Size = new System.Drawing.Size(64, 21);
+            this.ScanDir.TabIndex = 13;
+            this.ScanDir.Text = "扫描目录";
+            this.ScanDir.UseVisualStyleBackColor = true;
+            this.ScanDir.Click += new System.EventHandler(this.ScanDir_Click);
+            // 
+            // FileList
+            // 
+            this.FileList.FormattingEnabled = true;
+            this.FileList.ItemHeight = 12;
+            this.FileList.Location = new System.Drawing.Point(9, 70);
+            this.FileList.Margin = new System.Windows.Forms.Padding(2);
+            this.FileList.Name = "FileList";
+            this.FileList.Size = new System.Drawing.Size(257, 112);
+            this.FileList.TabIndex = 14;
+            this.FileList.SelectedIndexChanged += new System.EventHandler(this.FileList_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(275, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 12);
+            this.label1.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(106, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 20);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "关于 && 帮助";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // 轻音
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(274, 202);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FileList);
+            this.Controls.Add(this.ScanDir);
+            this.Controls.Add(this.AudioStaus);
+            this.Controls.Add(this.PlaybackProgress);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.StopMusic);
+            this.Controls.Add(this.PlayMusic);
+            this.Controls.Add(this.isRandom);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.OutputDeviceList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "轻音";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "轻音";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox OutputDeviceList;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox isRandom;
+        private System.Windows.Forms.Button PlayMusic;
+        private System.Windows.Forms.Button StopMusic;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar PlaybackProgress;
+        private System.Windows.Forms.Label AudioStaus;
+        private System.Windows.Forms.Button ScanDir;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListBox FileList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,273 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace KeyMusic
+{
+    public partial class 轻音 : Form
+    {
+
+        IList<Info> OutputDevice = new List<Info>(); //设置音频驱动列表
+        BindingList<MusicInfo> MusicList = new BindingList<MusicInfo>(); //音频列表
+        WaveOutEvent waveOutEvent = new WaveOutEvent() { DeviceNumber = -1 }; //注册音频输出事件
+        int fileTime = 0; //当前选中文件的总时长
+        String NowPlayPath = ""; // 现在播放音频的目录
+        private const int WM_HOTKEY = 0x312; //窗口消息：热键
+        private const int WM_CREATE = 0x1; //窗口消息：创建
+        private const int WM_DESTROY = 0x2; //窗口消息：销毁
+        private const int PlaySound = 0x1111; //热键ID（播放音乐）
+        private const int StopSound = 0x2222; //热键ID （停止音乐）
+
+        public 轻音()
+        {
+            InitializeComponent();
+            BindDevice();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        public class Info
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+
+        }
+
+        public class MusicInfo
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+            public string FilePath { get; set; }
+        }
+
+
+        private string Sec2Min(Int64 sec)
+        {
+            Int64 fen;
+            Int64 miao;
+            String sMin;
+            String sSec;
+            if (sec < 0)
+                sec = 0;
+            miao = sec % 60;
+            sec = sec - miao;
+            sec /= 60;
+            fen = sec % 60;
+            sec -= fen;
+            if (fen < 10) { sMin = "0" + fen; } else { sMin = fen.ToString(); }
+            if (miao < 10) { sSec = "0" + miao; } else { sSec = miao.ToString(); }
+            return sMin + ":" + sSec;
+        }
+
+        private void BindDevice()
+        {
+            for (int n = 0; n < WaveOut.DeviceCount; n++)
+            {
+                var caps = WaveOut.GetCapabilities(n);
+                OutputDevice.Add(new Info() { Id = n.ToString(), Name = caps.ProductName });
+            }
+            OutputDeviceList.DataSource = OutputDevice;
+            OutputDeviceList.ValueMember = "Id";
+            OutputDeviceList.DisplayMember = "Name";
+        }
+
+
+
+
+        private void OnPlaying()
+        {
+            while (true)
+            {
+                if (waveOutEvent.PlaybackState == PlaybackState.Playing)
+                {
+                    int sec = (int)(waveOutEvent.GetPosition() / waveOutEvent.OutputWaveFormat.BitsPerSample / waveOutEvent.OutputWaveFormat.Channels * 8.0 / waveOutEvent.OutputWaveFormat.SampleRate);
+                    try
+                    {
+                        Invoke(new updateProgressDelegate(updateProgress), new Object[] { sec });
+                    }
+                    catch
+                    {
+                        return;
+                    }
+
+                }
+                else
+                {
+
+                    try { Invoke(new updateProgressDelegate(updateProgress), new Object[] { -1 }); }
+                    catch { return; }
+                }
+                Thread.Sleep(100);
+            }
+        }
+
+
+        delegate void updateProgressDelegate(int current);
+
+        void updateProgress(int current)
+        {
+            if (current == -1)
+            {
+                PlaybackProgress.Value = 0;
+                AudioStaus.Text = "00:00/00:00";
+                return;
+            }
+            int staus = (int)((current * 100 / fileTime));
+            PlaybackProgress.Value = staus;
+            AudioStaus.Text = Sec2Min(current) + "/" + Sec2Min(fileTime);
+        }
+
+        private void StartPlay()
+        {
+            if (NowPlayPath == "")
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "MP3|*.mp3";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    NowPlayPath = dialog.FileName;
+                }
+                else
+                {
+                    MessageBox.Show("未指定音频文件");
+                    return;
+                }
+            }
+            if (isRandom.Checked && FileList.Items.Count > 0)
+            {
+                Random rd = new Random();
+                int i = rd.Next(0, FileList.Items.Count);
+                FileList.SelectedIndex = i;
+            }
+
+            waveOutEvent.DeviceNumber = OutputDeviceList.SelectedIndex;
+            //文件访问对象
+            AudioFileReader waveReader = new AudioFileReader(NowPlayPath);
+            fileTime = (int)waveReader.TotalTime.TotalSeconds;
+            if (waveOutEvent.PlaybackState == PlaybackState.Playing)
+            {
+                waveOutEvent.Stop();
+            }
+            waveOutEvent.Init(waveReader);
+            waveOutEvent.Play();
+            Thread thread = new Thread(new ThreadStart(OnPlaying));
+            thread.Start();
+        }
+
+        private void PlayMusic_Click(object sender, EventArgs e)
+        {
+            StartPlay();
+        }
+
+        private void StopMusic_Click(object sender, EventArgs e)
+        {
+            waveOutEvent.Stop();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //DialogResult result = MessageBox.Show("确认退出吗？", "退出询问", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            //if (result == DialogResult.Cancel) { waveOutEvent.Stop(); e.Cancel = true; }
+            //else { e.Cancel = false; }
+            waveOutEvent.Stop();
+        }
+
+
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            switch (m.Msg)
+            {
+                case WM_HOTKEY: //窗口消息-热键ID
+                    switch (m.WParam.ToInt32())
+                    {
+                        case PlaySound:
+                            StartPlay();
+                            break;
+                        case StopSound:
+                            waveOutEvent.Stop();
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case WM_CREATE: //窗口消息-创建
+                    AppHotKey.RegKey(Handle, StopSound, AppHotKey.KeyModifiers.Alt, Keys.Subtract);
+                    AppHotKey.RegKey(Handle, PlaySound, AppHotKey.KeyModifiers.Alt, Keys.Add);
+                    break;
+                case WM_DESTROY: //窗口消息-销毁
+                    AppHotKey.UnRegKey(Handle, StopSound); //销毁热键
+                    AppHotKey.UnRegKey(Handle, PlaySound); //销毁热键
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void ScanDir_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+
+                string[] files = Directory.GetFiles(dialog.SelectedPath, "*.mp3", SearchOption.AllDirectories);    //从“路径”中搜索所有的指定文件
+                SortedList<string, FileInfo> fileList = new SortedList<string, FileInfo>();
+                if (files.Length > 0)
+                {
+                    //搜索到了文件，继续执行
+
+                    foreach (string f in files)    //把文件夹中搜索到文件信息全部存储到刚才声明的字典中
+                    {
+                        FileInfo fi = new FileInfo(f);  //根据路径获取文件信息
+                        fileList.Add(fi.LastWriteTime.Ticks.ToString() + fi.Name, fi);  //存储文件信息到字典中
+                    }
+                    //把存储到文件信息字典中的数据显示到listview中
+                    int i = 0;
+                    MusicList.Clear();
+                    foreach (string item in fileList.Keys)
+                    {
+                        MusicList.Add(new MusicInfo() { Id = i.ToString(), Name = fileList[item].Name, FilePath = fileList[item].FullName });
+                        i++;
+                    }
+                    FileList.DataSource = MusicList;
+                    FileList.ValueMember = "Id";
+                    FileList.DisplayMember = "Name";
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("未查找到文件");
+            }
+        }
+
+        private void FileList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            NowPlayPath = MusicList[FileList.SelectedIndex].FilePath.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+            Application.Restart();
+        }
+
+        private void PlayMusic_MouseMove(object sender, MouseEventArgs e)
+        {
+            toolTip1.SetToolTip(PlayMusic, "快捷键:Alt键+加号键");
+        }
+
+        private void StopMusic_MouseMove(object sender, MouseEventArgs e)
+        {
+            toolTip2.SetToolTip(StopMusic, "快捷键:Alt键+减号键");
+        }
+    }
+}
